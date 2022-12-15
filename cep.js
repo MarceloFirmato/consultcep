a5567ed2078866fcffa3ee5dbb8aefc30885759c
MarceloFirmato <116672694+MarceloFirmato@users.noreply.github.com>
2022-12-15 12:29:30 -0300

    não está consultando

---
+++ cep.js
@@ -0,0 +1,41 @@
+function criarElementoResultadoSucesso(value) {
+    const result = document.querySelector('#result')
+    result.innerHTML= ""
+    if(!!value.cep){
+        for (const property in value) {
+            result.insertAdjacenthtml("beforeend"
+            `<li> ${property}: ${value[property]}<li/>`
+            )
+        }
+    } else {
+        criarElementoResultadoErro("CEP não encontrado!")
+    }
+}
+
+function criarElementoResultadoErro(value){
+    const result =document.querySelector('#result')
+    result.innerHTML=""
+    result.insertAdjacentHTML("beforeend", `<h2 style="color: #F00">${value}</h2>`
+    );
+}
+
+function pesquisaCep(cep) {
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    fetch(url)
+    .then((response) => response.json())
+    .then((result) => {criarElementoResultadoSucesso(result)})
+    .catch((err) => {criarElementoResultadoErro("CEP inválido!!")})
+}
+
+const form = document.querySelector("form")
+const inputCEP = document.querySelector('#cep')
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    const cep = inputCEP.value.replace(/\G/g, "")
+    if (/^[0-9]{8}$/.test(cep)){
+        pesquisaCep(cep)
+    } else {
+        criarElementoResultadoErro("CEP inválido!!!")
+    }
+})
